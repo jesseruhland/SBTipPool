88aded7fb89ef416f377a4bde014106e174e190c
Jesse Ruhland <jesse.ruhland@gmail.com>
2022-08-02 15:24:30 -0400

    servers.test.js functional test

--- servers.test.js
+++ servers.test.js
@@ -11,7 +11,25 @@ describe("Servers test (with setup and tear-down)", function() {
     expect(allServers['server' + serverId].serverName).toEqual('Alice');
   });
 
+  it('should add a new server to the server table on updateServerTable()', () =>{
+    submitServerInfo();
+    updateServerTable();
+    let tableList = document.querySelectorAll('#serverTable tbody tr td')
+    expect(tableList.length).toEqual(2);
+    expect(tableList[0].innerText).toEqual('Alice');
+    expect(tableList[1].innerText).toEqual('$0.00');
+  })
+
+  it('should not allow a blank server name submission', () =>{
+    serverNameInput.value = '';
+    submitServerInfo();
+    expect(Object.keys(allServers).length).toEqual(0);
+  })
+
   afterEach(function() {
     // teardown logic
+    serverTbody.innerHTML = '';
+    allServers = {};
+    serverId = 0;
   });
 });
